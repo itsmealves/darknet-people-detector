df938408d5c15ef927acec38b56a92d6e8ac5a8b
Gabriel Alves <Gabriel Alves>
2018-01-07 14:30:44 -0300

    init

---
+++ cmake-build-debug/CMakeFiles/vitrine.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vitrine.dir/main.cpp.o"
+  "vitrine.pdb"
+  "vitrine"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vitrine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
